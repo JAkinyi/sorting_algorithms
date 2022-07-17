309de9787183cf0c53af7d68911bf891edff7e86
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-07-17 16:29:27 +0300

    Create 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array using the selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array;
+ */
+
+void selection_sort(int *array, size_t size)
+{
+  int i, j, min,temp;
+  
+  for (i = 0; i < size; i++)
+    {
+      min = i;
+      
+      for (j = i + 1; j < size; j++)
+        {
+          if (array[j] < array[min])
+          {
+            min = j;
+          }
+        }
+    
+        temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    
+        if (min != 0)
+        { 
+          print_array(array, size);
+        }   
+    }
+}
